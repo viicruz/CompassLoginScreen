27fe62535def678c581242d0431142c337105491
jacrifla <jaquelini_cris@hotmail.com>
2023-05-22 21:23:27 -0300

    alterado estilos da tela login e adicionado componente title

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -7,6 +7,7 @@ import Input from "../../components/common/Input";
 import Footer from "../../components/common/Footer";
 
 import { styles } from "./styles";
+import Title from "../../components/common/Title";
 
 type UserCredential = {
   email: string;
@@ -38,47 +39,47 @@ export default function Login() {
   }, [isCredentialValid]);
 
   return (
-    <View style={styles.loginScreen}>
-      <View>
-        <Text style={styles.textContainer}>Welcome</Text>
-      </View>
-      <View style={styles.inputHolder}>
-        <Input
-          //Must contain "@, ." and minimum of 5 characters.
-          isWrong={!isCredentialValid.email}
-          wrongText="Please enter a valid email address"
-          onChange={(e) => {
-            setUserCredential({
-              ...userCredential,
-              email: e,
-            });
+    <View style={styles.container}>
+      <Title title="Welcome" />
+      <View style={styles.inputContainer}>
+          <Input
+            //Must contain "@, ." and minimum of 5 characters.
+            isWrong={!isCredentialValid.email}
+            wrongText="Please enter a valid email address"
+            onChange={(e) => {
+              setUserCredential({
+                ...userCredential,
+                email: e,
+              });
+            }}
+            icon="Mail"
+            placeholder="Your Email"
+          />
+
+          <Input
+            //Minimum 6 characters and 1 letter.
+            isWrong={!isCredentialValid.password}
+            wrongText="Please enter a valid password"
+            onChange={(e) => {
+              setUserCredential({
+                ...userCredential,
+                password: e,
+              });
+            }}
+            secureTextEntry
+            icon="Lock"
+            placeholder="Your Password"
+          />
+        
+        <Button
+          onPress={() => {
+            if (isCredentialValid.email && isCredentialValid.password) 
+              navigation.navigate("Home" as never);
+            
           }}
-          icon="Mail"
-          placeholder="Your Email"
-        />
-        <Input
-          //Minimum 6 characters and 1 letter.
-          isWrong={!isCredentialValid.password}
-          wrongText="Please enter a valid password"
-          onChange={(e) => {
-            setUserCredential({
-              ...userCredential,
-              password: e,
-            });
-          }}
-          secureTextEntry
-          icon="Lock"
-          placeholder="Your Password"
+          name="login"
         />
       </View>
-      <Button
-        onPress={() => {
-          if (isCredentialValid.email && isCredentialValid.password) 
-            navigation.navigate("Home" as never);
-          
-        }}
-        name="LOGIN"
-      />
       <Footer
         goTo="SignUp"
         textName="Don't have an account? "
