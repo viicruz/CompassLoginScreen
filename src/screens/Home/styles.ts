87d882104387a0b4cf1014eec23420665bd800c9
jacrifla <jaquelini_cris@hotmail.com>
2023-05-23 19:06:22 -0300

    style: alterado a estilizacao

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,23 +1,23 @@
 import { StyleSheet } from "react-native";
+import { colors } from "../../constants/theme";
 
 export const styles = StyleSheet.create({
   Container: {
-    backgroundColor: "#2D2D2D",
+    backgroundColor: colors.background,
+    alignItems: 'center',
+    justifyContent: 'center',
     flex: 1,
   },
-  HomeTitle: {
-    color: "#D78F3C",
-    fontWeight: "500",
-    fontSize: 20,
-    marginLeft: 26,
-    marginTop: 26,
-    marginBottom: 100,
-  },
+  // HomeTitle: {
+  //   color: colors.primary,
+  //   fontWeight: "500",
+  //   fontSize: 20,
+  //   marginLeft: 26,
+  //   marginTop: 26,
+  //   marginBottom: 100,
+  // },
 
   flatListContainer: {
-    width: "100%",
-    height: "100%",
-    flex: 1,
-    backgroundColor: "red",
+    // paddingHorizontal: 5
   },
 });
