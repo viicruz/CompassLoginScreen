9b782315ac5debcd6a01c8db6749b983c48c7a23
jacrifla <jaquelini_cris@hotmail.com>
2023-05-24 18:39:22 -0300

    estilizacao da tela

--- src/screens/ShoppingCart/index.tsx
+++ src/screens/ShoppingCart/index.tsx
@@ -1,4 +1,16 @@
-import {View, Text} from 'react-native';
-export default function ShoppingCart(){
-   return <View><Text>Ops, Empty Cart :( </Text></View>
+import { View, Text } from 'react-native';
+import { styles } from './styles';
+import ButtonBuyAdd from '../../components/common/ButtonBuyAdd';
+export default function ShoppingCart() {
+   return (
+      <View style={styles.container}>
+         <View style={styles.containerArea}>
+            <Text style={styles.warning}>Ops, Empty Cart :( </Text>
+            <Text style={styles.text}>Add a product </Text>
+         </View>
+         <View style={styles.buttonPlace}>
+            <ButtonBuyAdd label='buy' onPress={() => { }} />
+         </View>
+      </View>
+   )
 }
---
+++ src/screens/ShoppingCart/styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../constants/theme";
+
+export const styles = StyleSheet.create({
+    container: {
+        height: '100%',
+        alignItems: 'center',
+        backgroundColor: colors.background,
+        
+    },
+    containerArea: {
+        display: "none",
+        marginVertical: 150,   
+    },
+    warning: {
+        fontSize: 32,
+        fontWeight: '600',
+        color: colors.light,
+    },
+    text: {
+        fontSize: 20,
+        color: colors.light,
+        textAlign: 'center',
+    },
+    buttonPlace: {
+        position: "absolute",
+        bottom: 35,
+    }
+})
